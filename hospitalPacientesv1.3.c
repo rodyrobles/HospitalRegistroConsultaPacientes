0517e41b509d265725ca1060527be4c7bcc6ae8e
rodyrobles <rodyrob@gmail.com>
2018-03-06 22:52:31 -0600

    all validations done

--- hospitalPacientesv1.3.c
+++ hospitalPacientesv1.3.c
@@ -23,8 +23,8 @@ struct paciente {
 
 int main(){
 
-	int  i=0, registro=0, encontrado, controlCiclo, validaNumero, selec1, selec2, selec3;
-	char nombreRegistrado[50], nombreBuscado[50];
+	int  i=0, registro=0, encontrado, controlCiclo, validaNumero, a, d, selec1, selec2, selec3;
+	char nombreRegistrado[50], nombreBuscado[50], validaNombre[50];
 	char validaSexo;
 	
 	system ("color 0a");
@@ -78,33 +78,64 @@ int main(){
 					} else {
 					
 						printf("\tIngresa los datos del paciente #%.3d\n\n", registro+1);
-						printf("\tNombre y apellido paterno: ");
-						fflush(stdin); //limpia buffer del teclado
-						gets(pacientes[registro].nombreApellido);
-						
-						//nueva instruccion para edad inicio
+												
+						//-----Valida de nombre inicio-----
+						do {
+							controlCiclo=0;
+							
+							printf("\tNombre y apellido paterno: ");
+							fflush(stdin); //limpia buffer del teclado
+							gets(validaNombre);
+							a=0;
+							d=0;
+													
+						for (i=0; validaNombre[i]; i++) {
+							if (validaNombre[i]>='a' && validaNombre[i]<='z' || validaNombre[i]>='A' && validaNombre[i]<='Z') {
+								a=1;
+							}
+									
+							if  (validaNombre[i]>='0' && validaNombre[i]<='9') {
+								d=1;
+							}
+						}
+									
+							if (d==1) {
+								lowvideo();textbackground(RED);
+								printf("\t- Error! Ingrese unicamente letras -\n");
+								lowvideo();textbackground(BLACK);
+							}
+							
+							if (a==1 && d==0)
+							{
+								strcpy(pacientes[registro].nombreApellido,validaNombre);
+								controlCiclo=1;
+								
+							}
+						} while (controlCiclo==0);
+						//-----Valida de nombre fin-----
+													
+						//-----valida ingreso edad inicio-----
 						do {
 							controlCiclo=0;
 							printf("\tEdad: ");
-							
-								
+															
 							if (scanf("%i",&pacientes[registro].edad)!=1) {
-								
-								printf("\t- Error! Solo numeros -\n");
+								lowvideo();textbackground(RED);
+								printf("\t- Error! Ingrese unicamente numeros -\n");
+								lowvideo();textbackground(BLACK);
 								while (getchar() != '\n');
 							
 							}else {
 								controlCiclo=1;
 							}
 						
-						} while (controlCiclo==0);
+						} while (controlCiclo==0);						
+						//-----valida ingreso edad fin-----
 						
-						//nueva instruccion para edad fin
-						
-						//Nueva para sexo solo ingresa m/f---inicio---
+						//-----valida ingreso m/f inicio-----
 						do {													
 							controlCiclo=0;
-							printf("\tSexo (m/f): ");
+							printf("\tSexo (solo m, o f): ");
 							fflush(stdin); //limpia buffer del teclado	
 							scanf("%c",&validaSexo);
 														
@@ -112,38 +143,32 @@ int main(){
 								(pacientes[registro].sexo=validaSexo);
 								controlCiclo=1;
 							}else {
-								printf("\t- Error! solo m/f -\n");
+								lowvideo();textbackground(RED);
+								printf("\t- Error! ingrese unicamente m, o f -\n");
+								lowvideo();textbackground(BLACK);
 							}
 						} while (controlCiclo==0);
-						//Nueva para sexo solo ingresa m/f---fin---
-						
-						
-						//printf("\tCondicion del paciente, ingrese un numero\n");
-						//printf("\t1=Muy Leve 2=Leve 3=Grave 4=Muy grave 5=En extremo grave: ");
-						//scanf("%d", &pacientes[registro].condicion);
-						
-						
-						//-----OK-----Valida que solo se ingresen numeros-----inicio -----
+						//-----valida ingreso m/f fin-----
+												
+						//-----valida ingreso numeros 1 al 5 inicio-----
 						do {													
 							controlCiclo=0;
-							printf("\tCondicion del paciente, ingrese un numero\n");
+							printf("\tCondicion del paciente, ingrese solo un numero\n");
 							printf("\t1=Muy Leve 2=Leve 3=Grave 4=Muy grave 5=En extremo grave: ");
-								
-							if (scanf("%i",&pacientes[registro].edad)!=1)  {
-								
-								printf("\t- Error! solo numeros -\n");
+																						
+							if (scanf("%i",&pacientes[registro].condicion)!=1 && pacientes[registro].condicion<1 || pacientes[registro].condicion>5 )  {
+								lowvideo();textbackground(RED);
+								printf("\t- Error! Ingrese unicamente numeros del 1 al 5 -\n");
+								lowvideo();textbackground(BLACK);
 								while (getchar() != '\n');
 							
 							}else {
 								controlCiclo=1;
 							}
 						
-							} while (controlCiclo==0);
-																		
-						
-						//-----Valida que solo se ingrese numeros -----fin -----
-						
-						
+						} while (controlCiclo==0);
+						//-----valida ingreso numeros 1 al 5 fin-----
+											
 						printf("\n\tRegistro exitoso\n\n");
 						registro++;		
 						}
@@ -188,7 +213,7 @@ int main(){
 					printf("Ingresa Nombre y apellido paterno: ");
 					fflush(stdin); //limpia buffer del teclado
 					gets(nombreBuscado);  
-					///Busqueda -------------------------------------
+					//-----Busqueda inicio-----
 					encontrado=0;
 					for (i=0; i<10; i++) {
 						strcpy(nombreRegistrado,pacientes[i].nombreApellido);
@@ -207,7 +232,7 @@ int main(){
 						if (encontrado==0) printf("\n\tNO hay registros con este nombre");
 						
 						
-					///Busqueda -------------------------------------
+					//-----Busqueda fin-----
 					printf("\n\n");
 					system ("pause");
 					break;
